79b73507db4c4a322d248b1892067ac3f9dcd0e3
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-25 16:57:53 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,31 @@ describe "#addition" do
 end
 
 def subtraction(num1, num2)
-
+describe "subtraction"do
+  it "subtracts two numbers together"do
+    expect(subtraction(5,4)).to eq(1)
 end
 
 def division(num1, num2)
-
+describe "division" do
+  it "divides two numbers together" do
+    expect(division(50,5)).to eq(10)
 end
 
 def multiplication(num1, num2)
-
+describe "multiplication" do
+  it "multiplies two numbers together" do
+    expect(multiplication(10,5)).to eq(50)
 end
 
 def modulo(num1, num2)
-
+  describe "modulo" do
+    it "returns the remainder of two numbers" do
+    expect(modulo(34, 5)).to eq(4)
 end
 
 def square_root(num)
-
+  describe "square_root" do
+    it "returns the square root of a number" do
+    expect(square_root(81)).to eq(9)
 end
